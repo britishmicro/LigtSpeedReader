1363375001e8958cddc2bcd2c7b6f910e50d5c17
John Powell <john.powell@britishmicro.com>
2015-09-17 18:18:36 +0100

    Names changes

--- LigtSpeedReaderLite/Properties/AssemblyInfo.cs
+++ LigtSpeedReaderLite/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LigtSpeedReaderLite")]
+[assembly: AssemblyTitle("LightSpeedReader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Symantec Corporation")]
-[assembly: AssemblyProduct("LigtSpeedReaderLite")]
-[assembly: AssemblyCopyright("Copyright © Symantec Corporation 2013")]
+[assembly: AssemblyCompany("BritishMicro")]
+[assembly: AssemblyProduct("LightSpeedReader")]
+[assembly: AssemblyCopyright("Copyright © Britishmicro 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- LigtSpeedReaderLite/VisualReader.Designer.cs
+++ LigtSpeedReaderLite/VisualReader.Designer.cs
@@ -49,6 +49,7 @@
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -60,8 +61,9 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel2);
-            this.splitContainer1.Size = new System.Drawing.Size(838, 501);
-            this.splitContainer1.SplitterDistance = 172;
+            this.splitContainer1.Size = new System.Drawing.Size(1117, 617);
+            this.splitContainer1.SplitterDistance = 211;
+            this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 0;
             this.splitContainer1.TabStop = false;
             // 
@@ -70,16 +72,17 @@
             this.tableLayoutPanel1.ColumnCount = 3;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 3.304774F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 96.69523F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 51F));
             this.tableLayoutPanel1.Controls.Add(this.textboxWordDisplay, 1, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 39.62264F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60.37736F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(838, 172);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1117, 211);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
             // textboxWordDisplay
@@ -90,13 +93,14 @@
             this.textboxWordDisplay.Enabled = false;
             this.textboxWordDisplay.Font = new System.Drawing.Font("Cambria", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textboxWordDisplay.ForeColor = System.Drawing.SystemColors.Window;
-            this.textboxWordDisplay.Location = new System.Drawing.Point(29, 62);
+            this.textboxWordDisplay.Location = new System.Drawing.Point(39, 76);
+            this.textboxWordDisplay.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textboxWordDisplay.Multiline = true;
             this.textboxWordDisplay.Name = "textboxWordDisplay";
-            this.textboxWordDisplay.Size = new System.Drawing.Size(768, 83);
+            this.textboxWordDisplay.Size = new System.Drawing.Size(1022, 102);
             this.textboxWordDisplay.TabIndex = 1;
             this.textboxWordDisplay.TabStop = false;
-            this.textboxWordDisplay.Text = "LightSpeedReader Lite";
+            this.textboxWordDisplay.Text = "LightSpeedReader";
             this.textboxWordDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // tableLayoutPanel2
@@ -104,34 +108,37 @@
             this.tableLayoutPanel2.ColumnCount = 3;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 0.9673519F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 99.03265F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 12F));
             this.tableLayoutPanel2.Controls.Add(this.panel1, 1, 2);
             this.tableLayoutPanel2.Controls.Add(this.textboxToSpeak, 1, 1);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 3;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 2.702703F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 97.29729F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(838, 325);
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1117, 401);
             this.tableLayoutPanel2.TabIndex = 2;
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(11, 299);
+            this.panel1.Location = new System.Drawing.Point(14, 370);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(810, 22);
+            this.panel1.Size = new System.Drawing.Size(1080, 27);
             this.panel1.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Location = new System.Drawing.Point(5, 5);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(745, 13);
+            this.label1.Size = new System.Drawing.Size(999, 17);
             this.label1.TabIndex = 3;
             this.label1.Text = "F5 to Play and Pause, Alt-F5 to stop and return to start, Alt-right arrow to spee" +
     "d up, Alt-left arrow to slow down. Alt-Insert replaces contents with clipboard t" +
@@ -145,21 +152,23 @@
             this.textboxToSpeak.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textboxToSpeak.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textboxToSpeak.HideSelection = false;
-            this.textboxToSpeak.Location = new System.Drawing.Point(11, 11);
+            this.textboxToSpeak.Location = new System.Drawing.Point(14, 13);
+            this.textboxToSpeak.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textboxToSpeak.Multiline = true;
             this.textboxToSpeak.Name = "textboxToSpeak";
             this.textboxToSpeak.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textboxToSpeak.Size = new System.Drawing.Size(815, 282);
+            this.textboxToSpeak.Size = new System.Drawing.Size(1086, 349);
             this.textboxToSpeak.TabIndex = 0;
             this.textboxToSpeak.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textboxToSpeak_KeyUp);
             // 
             // VisualReader
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(838, 501);
+            this.ClientSize = new System.Drawing.Size(1117, 617);
             this.Controls.Add(this.splitContainer1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "VisualReader";
